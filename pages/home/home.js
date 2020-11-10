b9f6d872c90c2d47dbc507048a7a68d0783f9f0c
yangqinzhu <yqzsszl@163.com>
2020-11-10 14:23:28 +0800

    03小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,9 +5,33 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    name: 'hello world',
+    age: 18,
+    students: [
+      {id: 110, name: 'tom', age: 30},
+      {id: 111, name: 'jerry', age: 33},
+      {id: 112, name: 'james', age: 35},
+      {id: 113, name: 'jay', age: 37},
+    ],
+    counter: 0
   },
+  handleBtnClick() {
+    // 错误方法，界面不会刷新
+    this.data.counter++
 
+    // this。setData()
+    // if(tag != 0){
+      this.setData({
+        counter: this.data.counter--
+      })
+    // }
+    // else{
+    //   this.setData({
+    //     counter: this.data.counter++
+    //   })
+    // }
+  },
+  
   /**
    * 生命周期函数--监听页面加载
    */
