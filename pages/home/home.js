d999b5f86070e00f0d7572c05ca0660cd36aab27
yangqinzhu <yqzsszl@163.com>
2020-11-26 10:23:27 +0800

    事件相关处理

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,16 +5,68 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    titles: ['衣服', '裤子', '鞋子']
+  },
+  handleBtnClick() {
+    console.log('点击了按钮')
+  },
+  handleTouchMove() {
+    console.log('handleTouchMove');
+  },
+  handleTouchEnd() {
+    console.log('handleTouchEnd');
+  },
+  handleLongPress() {
+    console.log('handleLongPress');
+  },
+  handleTap() {
+    console.log('handleTap');
+  },
+  handleTouchStart() {
+    console.log('handleTouchStart');
+  },
+  handleEventClick(event) {
+    console.log('----------', event)
+  },
+  handleInner(event) {
+    console.log(event)
+  },
+  handleOuter(event) {
+    console.log(event)
+  },
+  handleItemClick(event) {
+    console.log(event)
+    const dataset = event.currentTarget.dataset;
+    const title = dataset.item;
+    const index = dataset.index;
+    console.log(title, index)
   },
 
+  // 事件冒泡和事件捕获
+  handleCaptureView1(event) {
+    console.log('handleCaptureView1')
+  },
+  handleBindView1(event) {
+    console.log('handleBindView1')
+  },
+  handleCaptureView2(event) {
+    console.log('handleCaptureView2')
+  },
+  handleBindView2(event) {
+    console.log('handleBindView2')
+  },
+  handleCaptureView3(event) {
+    console.log('handleCaptureView3')
+  },
+  handleBindView3(event) {
+    console.log('handleBindView3')
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
 
   },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
